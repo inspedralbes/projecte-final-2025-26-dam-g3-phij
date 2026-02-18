a9f73a929bed05c5332986e469a144bdab07e1c1
a24gaujaghar <a24gaujaghar@inspedralbes.cat>
2026-02-18 08:25:13 +0100

    Router

--- Valkrypt-RPG/frontend/src/router.js
+++ Valkrypt-RPG/frontend/src/router.js
@@ -1,14 +1,40 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from './views/HomeView.vue'
 import LoginView from './views/LoginView.vue'
+import RegisterView from './views/RegisterView.vue' 
+import VerifyView from './views/VerifyView.vue'
+import UserPage from './views/UserPage.vue'
 
 const routes = [
-  { path: '/', name: 'home', component: HomeView },
-  { path: '/login', name: 'login', component: LoginView }
+  { 
+    path: '/', 
+    name: 'home', 
+    component: HomeView 
+  },
+  { 
+    path: '/login', 
+    name: 'login', 
+    component: LoginView 
+  },
+  { 
+    path: '/register',
+    name: 'register', 
+    component: RegisterView 
+  },
+  { 
+    path: '/verify', 
+    name: 'verify', 
+    component: VerifyView 
+  },
+  { 
+    path: '/UserPage',
+    name: 'userpage', 
+    component: UserPage 
+  }
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes
 })
 
